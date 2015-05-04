2590a9b0dea21b18a6c3cdc5e225429f7482d7a7
ElectricMaxxx <maximilian.berghoff@gmx.de>
2015-05-04 22:24:16 +0200

    divided in both routes with an ui-view

--- stetp_13_routing/app.js
+++ stetp_13_routing/app.js
@@ -1,6 +1,21 @@
-angular.module("myModule", ['restangular', 'ngDraggable'])
-    .config([
-        "RestangularProvider", function(RestangularProvider) {
+angular.module("myModule", ['restangular', 'ngDraggable', 'ui.router'])
+    .config(["RestangularProvider", "$stateProvider", function(RestangularProvider, $stateProvider) {
             RestangularProvider.setRequestSuffix('.json');
             return RestangularProvider.setBaseUrl("/ExampleApp/step_08_backend/api/v1/");
-        }]);
+    }])
+    .config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/type');
+        $stateProvider
+            .state('type', {
+                url: '/type',
+                views: {
+                    "type-view": {templateUrl: 'views/type_home.tpl.html', controller: 'TypeController as typeController'}
+                }
+            })
+            .state('map', {
+                url: '/map',
+                views: {
+                    "type-view": {templateUrl: 'views/type_map_home.tpl.html', controller: 'TypeController as typeController'}
+                }
+            })
+    }]);
